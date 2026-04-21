81d46d9b37a3f7c5d38937e8b1ad8bbfc3e4ec94
ChrisLozano86 <chris.lozano8603@gmail.com>
2026-04-21 14:39:37 -0600

    cecybot_2026 v1.1

--- admin/class/Registro.php
+++ admin/class/Registro.php
@@ -253,7 +253,7 @@ class Registro {
 
     public static function recuperarTodos() {
         $conexion = new Conexion();
-        $consulta = $conexion->prepare('SELECT * FROM ' . self::TABLA);
+        $consulta = $conexion->prepare('SELECT * FROM ' . self::TABLA. ' ORDER BY id ASC');
         $consulta->execute();
         $registros = $consulta->fetchAll();
   
